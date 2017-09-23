ac2ebdb0d1b9ccde44cb8f72f3f6f97823490926
Daniel <Coal0@users.noreply.github.com>
2017-09-23 20:53:56 +0200

    Updated setup.py
    * PEP8-compliance
    * Wrapped imports into a `try` / `except` statement
    * `install` function is now bare `setup` call

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 import os.path
 import re
 
-from setuptools import setup, find_packages
+try:
+	from setuptools import setup, find_packages
+except ImportError:
+	from distutils.core import setup, find_packages
+
 
 def get_file(*paths):
     path = os.path.join(*paths)
@@ -10,17 +14,19 @@ def get_file(*paths):
             return f.read().decode('utf8')
     except IOError:
         pass
+
+
 def get_version():
     init_py = get_file(os.path.dirname(__file__), 'prng_stego', '__init__.py')
     pattern = r"{0}\W*=\W*'([^']+)'".format('__version__')
-    version, = re.findall(pattern, init_py)
+    version = re.findall(pattern, init_py)
     return version
 
-def install():
-	setup(
-		name='prng_stego',                      
-		version=get_version(),                    
-		packages = find_packages(exclude=['tester','tester_mac']),
-		scripts=['prng_stego'],
-		install_requires=['Pillow','cryptography','numpy'],             
-      )
+
+setup(
+	name='prng_stego',                      
+	version=get_version(),                    
+	packages = find_packages(exclude=['tester','tester_mac']),
+	scripts=['prng_stego'],
+	install_requires=['Pillow','cryptography','numpy'],             
+)
